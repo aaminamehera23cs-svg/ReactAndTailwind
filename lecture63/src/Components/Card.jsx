e5666d7bc9bd59fd3b3c65da73cef2652d57f1b4
aaminamehera23cs-svg <aaminamehera23cs@rnsit.ac.in>
2026-07-07 16:26:48 +0530

    Add lecture63

---
+++ lecture63/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+      {props.children}//allows for accessing children components
+    </div>
+  )
+}
+
+export default Card
